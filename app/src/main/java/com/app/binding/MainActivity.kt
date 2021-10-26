e25c4df011f3673bdb4b36e36f0b6ef04ca825d8
Aditya Singh <68631004+adisingh925@users.noreply.github.com>
2021-10-26 16:03:41 +0530

    data binding

---
+++ app/src/main/java/com/app/binding/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.app.binding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.binding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val myuser = Model("aditya",1,false)
+
+        binding.user = myuser
+    }
+}
---
+++ app/src/main/java/com/app/binding/Model.kt
@@ -0,0 +1,7 @@
+package com.app.binding
+
+data class Model(
+    val name:String,
+    val rno:Int,
+    val status:Boolean
+)
